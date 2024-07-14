4ca7ffaf4cb4cfd0ef5d4252fd4b934b527473ac
Caleb Owens <hello@calebowens.com>
2024-07-14 14:04:21 +0200

    Installed prettier

---
+++ .prettier.config.mjs
@@ -0,0 +1,11 @@
+/**
+ * @see https://prettier.io/docs/en/configuration.html
+ * @type {import("prettier").Config}
+ */
+const config = {
+	semi: false,
+	useTabs: true,
+	plugins: ["prettier-plugin-ejs"],
+}
+
+export default config
--- scripts/build.js
+++ scripts/build.js
@@ -6,8 +6,8 @@ import { compile } from "sass-embedded"
 console.log("Clearing build directory")
 
 if (existsSync("./output")) {
-    rmSync("./output", { recursive: true })
-    mkdirSync("./output")
+	rmSync("./output", { recursive: true })
+	mkdirSync("./output")
 }
 
 console.log("Rendering HTML")
@@ -15,18 +15,22 @@ console.log("Rendering HTML")
 const pages = glob.globSync("src/**/*.page.ejs")
 
 pages.forEach((pagePath) => {
-    renderFile(pagePath, (error, result) => {
-        if (error) {
-            console.error(`Failed to render page ${pagePath}`, error)
-        }
+	renderFile(pagePath, (error, result) => {
+		if (error) {
+			console.error(`Failed to render page ${pagePath}`, error)
+		}
 
-        const newPath = pagePath.replace(/\.page\.ejs$/, '.html').split('/').slice(1).join('/')
-        const directory = pagePath.split('/').slice(1, -1).join('/')
+		const newPath = pagePath
+			.replace(/\.page\.ejs$/, ".html")
+			.split("/")
+			.slice(1)
+			.join("/")
+		const directory = pagePath.split("/").slice(1, -1).join("/")
 
-        mkdirSync(`./output/${directory}`, { recursive: true })
-        writeFileSync(`./output/${newPath}`, result)
-        console.log(`Wrote ./output/${newPath} successfully`)
-    })
+		mkdirSync(`./output/${directory}`, { recursive: true })
+		writeFileSync(`./output/${newPath}`, result)
+		console.log(`Wrote ./output/${newPath} successfully`)
+	})
 })
 
 console.log("Rendering SCSS")
@@ -34,16 +38,20 @@ console.log("Rendering SCSS")
 const stylesheets = glob.globSync("src/**/*.page.scss")
 
 stylesheets.forEach((stylesheetPath) => {
-    try {
-        const result = compile(stylesheetPath)
+	try {
+		const result = compile(stylesheetPath)
 
-        const newPath = stylesheetPath.replace(/\.page\.scss$/, '.css').split('/').slice(1).join('/')
-        const directory = stylesheetPath.split('/').slice(1, -1).join('/')
+		const newPath = stylesheetPath
+			.replace(/\.page\.scss$/, ".css")
+			.split("/")
+			.slice(1)
+			.join("/")
+		const directory = stylesheetPath.split("/").slice(1, -1).join("/")
 
-        mkdirSync(`./output/${directory}`, { recursive: true })
-        writeFileSync(`./output/${newPath}`, result.css)
-        console.log(`Wrote ./output/${newPath} successfully`)
-    } catch (error) {
-        console.error(`Failed to render stylesheet ${stylesheetPath}`, error)
-    }
+		mkdirSync(`./output/${directory}`, { recursive: true })
+		writeFileSync(`./output/${newPath}`, result.css)
+		console.log(`Wrote ./output/${newPath} successfully`)
+	} catch (error) {
+		console.error(`Failed to render stylesheet ${stylesheetPath}`, error)
+	}
 })
